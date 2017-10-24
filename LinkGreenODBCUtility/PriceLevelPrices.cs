026426628bbd5a4b079829e0c24a4e9a81bd0e1a
Haden Hiles <hadenhiles@gmail.com>
2017-10-24 12:13:34 -0400

    Remove unused references

--- LinkGreenODBCUtility/PriceLevelPrices.cs
+++ LinkGreenODBCUtility/PriceLevelPrices.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Configuration;
-using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using DataTransfer.AccessDatabase;
 using LinkGreen.Applications.Common;
-using Common.Utilities;
 using LinkGreen.Applications.Common.Model;
 
 namespace LinkGreenODBCUtility
